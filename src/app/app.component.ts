a739176637464c70bfc8786e5d1fe5d6ff6ea24c
guhasayani <guhasayani.26@gmail.com>
2022-05-26 19:52:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'task2';
+  title = "";
+  convert(a:any,b:any){
+    let i:any="";
+    if(b=="f"){
+      i=(a-32)/1.8;
+      this.title=i+ "°C";
+    }
+    else{
+      i=(a*1.8+32);
+      this.title=i+ "°F";
+    }
+    
+  }
 }
